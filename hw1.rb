2c90a6cffc54ea0c592a7a3e329f12539f7ae0fd
Dmitry Shevchenko <dmishe@gmail.com>
2012-02-26 23:58:34 -0600

    HW1 initial

---
+++ hw1.rb
@@ -0,0 +1,107 @@
+require 'test/unit'
+
+class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+
+
+def palindrome?(string)
+  # determines whether a given word or phrase is a palindrome
+  cleaned = string.gsub(/\W/, '').downcase
+
+  cleaned == cleaned.reverse
+end
+
+def count_words(words)
+  # Given a string of input, return a hash whose keys are words in the string
+  # whose values are the number of times each word appears 
+
+  Hash[words.downcase.scan(/\w+/).group_by { |x|x }.map {|k,v| [k, v.length]}]
+end
+
+def rps_game_winner(players)
+  strategies = ["P", "R", "S"]
+  rules = [ # first wins
+    ["R", "S"],
+    ["S", "P"],
+    ["P", "R"]
+    ]
+
+  raise WrongNumberOfPlayersError unless players.length == 2
+  raise NoSuchStrategyError unless (players.map {|x| x[1]} - strategies).empty?
+  
+  player_strategies = players.map {|x| x[1]}
+  
+  return players[0] if player_strategies[0] == player_strategies[1]
+  
+  # see if we have player choices in rules, in reverse for second player
+  return players[0] if rules.include? player_strategies
+  return players[1] if rules.include? player_strategies.reverse
+end
+
+def rps_game_tournament(tournament)
+  # TODO: fix with recursion
+  rps_game_winner(tournament.map do |round|
+    rps_game_winner(round.map {|x| rps_game_winner(x)})    
+  end)  
+end
+
+
+class HW1Test < Test::Unit::TestCase
+  def test_palindrome
+    assert palindrome?("A man, a plan, a canal -- Panama")
+    assert palindrome?("Madam, I'm Adam!")
+    assert !palindrome?("Abracadabra")
+  end
+
+  def test_count_words
+    assert_equal(
+      {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1}, 
+      count_words("A man, a plan, a canal -- Panama"))
+
+    assert_equal(
+      {'doo' => 3, 'bee' => 2}, 
+      count_words("Doo bee doo bee doo"))
+  end
+  
+  def test_rps
+    assert_raise(WrongNumberOfPlayersError) { rps_game_winner([]) }
+    assert_raise(WrongNumberOfPlayersError) { rps_game_winner([1, 2, 3]) }
+    
+    assert_raise(NoSuchStrategyError) do 
+      rps_game_winner([[ "Armando", "A" ], [ "Dave", "S" ]])
+    end
+    assert_raise(NoSuchStrategyError) do 
+      rps_game_winner([[ "Armando", "P" ], [ "Dave", "A" ]])
+    end
+    
+    assert_equal(
+      ["Dave", "R"], 
+      rps_game_winner([[ "Armando", "S" ], [ "Dave", "R" ]]), 
+      "Rock beats Scissors")
+    assert_equal(
+      ["Armando", "S"], 
+      rps_game_winner([[ "Armando", "S" ], [ "Dave", "P" ]]),
+      "Scissors beats Paper")
+    assert_equal(
+      ["Armando", "P"], 
+      rps_game_winner([[ "Armando", "P" ], [ "Dave", "R" ]]))
+    
+    assert_equal(
+      ["Armando", "S"], 
+      rps_game_winner([[ "Armando", "S" ], [ "Dave", "S" ]]),
+      "First player wins stalemate")
+  end
+  
+  def test_rps_tournament
+    assert_equal(["Richard", "R"], rps_game_tournament([
+      [
+        [ ["Armando", "P"], ["Dave", "S"] ],
+        [ ["Richard", "R"],  ["Michael", "S"] ],
+      ],
+      [ 
+        [ ["Allen", "S"], ["Omer", "P"] ],
+        [ ["David E.", "R"], ["Richard X.", "P"] ]
+      ]
+      ]))
+  end
+end
